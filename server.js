6ec62b5b7139f6d5067916ebf63166f5c0792325
asitaru <andreisitaru93@gmail.com>
2017-03-18 22:48:30 +0200

    main implementation

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var moment = require('moment');
 
 var app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Hello World!');
+app.use(express.static(__dirname + '/app'));
+
+app.use('/:str', (req,res)=>{
+
+    var formatedDate = {
+            unix: null,
+            natural: null
+    };
+    var date;
+    if(!isNaN(req.params.str)){
+        console.log("unix");
+        date = moment.unix(req.params.str);
+    }else {
+        console.log("natural");
+        date = moment(new Date(req.params.str));
+    }
+
+    if(date.isValid()){
+        console.log("is valid");
+        formatedDate = {
+            unix: date.format('X'),
+            natural:date.format('MMMM DD YYYY')
+        };
+    }
+    res.send('<p style="color:#365d9b; margin-left:40px"><b>' + JSON.stringify(formatedDate) + '</b></p>');
+
+});
+
+app.use('/', (req,res) =>{
+    res.sendFile('/home/andrei/fcc/timestampMicroservice/index.html');
 });
 
 app.listen(3000, () => {
